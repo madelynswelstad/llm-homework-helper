d961f5bc675a021e8362eab783ab2067044e157e
Madelyn Swelstad <madelynswelstad@gmail.com>
2024-11-06 11:40:46 -0700

    cleaned up checking connection function

--- vectorStoreInteraction.py
+++ vectorStoreInteraction.py
@@ -12,23 +12,33 @@ collectionName = ''
 url = 'https://071a9cd4-154f-4464-acfe-bb7a4ecb9770.us-east4-0.gcp.cloud.qdrant.io:6333'
 headers = {"Authorization": "Bearer " + str(api_key)}
 
-payload = {
-    "name": "collection_name",
-    "vectors": {
-        "size": vectorDim,
-        "distance": "cosine"
-    }
-}
 
-response = requests.get(url, headers=headers)
+def checkConnectionToStore():
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        print("Connection successful!\n", response.json())
+    else:
+        print("Failed to connect:", response.status_code, '\n', response.text)
+
+def addData():
+    payload = {
+        "name": "collection_name",
+        "vectors": {
+            "size": vectorDim,
+            "distance": "cosine"
+        }
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        print("Connection successful!\n", response.json())
+    else:
+        print("Failed to connect:", response.status_code, '\n', response.text)
 
 # response = requests.post(url, json=payload, headers=headers) # send POST request
 
-if response.status_code == 200:
-    print("Collection created successfully!")
-    print(response.json())
-else:
-    print("Failed to create collection:", response.status_code)
-    print(response.text)
-
 # def getKNearestNeighbors(vectorizedQuery):
+
+checkConnectionToStore()
