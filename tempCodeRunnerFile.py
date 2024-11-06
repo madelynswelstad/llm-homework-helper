b3e7764cb055c01efe188eae61d42c2687f67606
Madelyn Swelstad <madelynswelstad@gmail.com>
2024-11-06 16:34:04 -0700

    more functions for vector store interaction

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-collection_name
+currentId
--- vectorStoreInteraction.py
+++ vectorStoreInteraction.py
@@ -7,6 +7,9 @@ load_dotenv()
 api_key = os.getenv('QDRANT_API_KEY')
 k = 3
 vectorDim = 4
+distanceMetric = 'Cosine'
+
+currentId = 0
 
 url = 'https://071a9cd4-154f-4464-acfe-bb7a4ecb9770.us-east4-0.gcp.cloud.qdrant.io:6333'
 headers = {"Authorization": "Bearer " + str(api_key),
@@ -23,13 +26,14 @@ def checkConnectionToStore():
     else:
         print("Failed to connect:", response.status_code, '\n', response.text)
 
+
 def createCollection(collectionName):
     payload = {
         "name": collectionName,
         "vectors": {
             "config": {
-                "size": 4,
-                "distance": "Cosine"
+                "size": vectorDim,
+                "distance": distanceMetric
             }
         }
     }
@@ -42,5 +46,50 @@ def createCollection(collectionName):
     else:
         print("Failed to create collection:", response.status_code, '\n', response.text)
 
+    
+def addDataPoint(collectionName, vectorizedDocument, currentId):
+    # if getCollectionInfo(collectionName)
+    print('size of vector:', len(vectorizedDocument))
+    payload = {
+        "points": [
+            {
+                "id": currentId,
+                "vector": vectorizedDocument
+            }
+        ]
+    }
+
+    response = requests.put(f"{url}/collections/{collectionName}/points", json=payload, headers=headers)
+
+    # print result
+    if response.status_code == 200:
+        currentId += 1
+        print(f"Added a point to {collectionName} collection successful!\n", response.json())
+    else:
+        print(f"Failed to add point to {collectionName} collection:", response.status_code, '\n', response.text)
+
+
+def getPointById(collectionName, pointId):
+    response = requests.get(f"{url}/collections/{collectionName}/points/{pointId}", headers=headers)
+
+    # print result
+    if response.status_code == 200:
+        print("Point retrieval successful!\nPoint data:", response.json())
+    else:
+        print(f"Failed to retrieve point with ID {pointId}:", response.status_code, '\n', response.text)
+
+def getCollectionPoints(collectionName):
+    response = requests.get(f"{url}/collections/{collectionName}/points", headers=headers)
+
+    # print result
+    if response.status_code == 200:
+        print("Point retrieval successful!\nPoint data:", response.json())
+    else:
+        print(f"Failed to get points:", response.status_code, '\n', response.text)
+
+
 # checkConnectionToStore()
-createCollection('testCollection')
+# createCollection('testCollection')
+addDataPoint('testCollection', [0.1, 0.2, 0.3, 0.4], currentId)
+getPointById('testCollection', 0)
+# getCollectionPoints('testCollection')
